aafa5c1ad8295964a709668870daa6645ae4969e
javlonbek556 <javlonzdess@gmail.com>
2025-04-14 12:32:42 +0500

    router codes located here

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PassportController;
 
 Route::get('/', function () {
     return view('auth.register');
@@ -11,13 +12,13 @@ Route::get('/', function () {
 
 
 Route::resource('/users',UserController::class)->names('users');
-Route::resource('/posts',PostController::class)->names('posts');
+Route::resource('/posts',PassportController::class)->names('posts');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-    Route::get( '/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::get('/posts/create', [PassportController::class, 'create'])->name('posts.create');
+    Route::get( '/posts/{post}/edit', [PassportController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PassportController::class, 'update'])->name('posts.update');
 });
 Route::middleware(['guest'])->group(function () {
     Route::get('/login', [UserController::class, 'index'])->name('users.index');
